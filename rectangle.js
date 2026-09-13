eaad56ea86df1029d86ffe199999bc3d85e1626a
_LauGhinG_FoX_ <markstrekov@gmail.com>
2026-09-13 21:04:44 +0300

    Add files via upload

--- rectangle.js
+++ rectangle.js
@@ -19,25 +19,31 @@ console.log(rectangele.p());
 
 
 class Book {
-    constructor(name, author, year, pages, isAllowed) {
+    constructor(name, author, year, pages, isAllowed, ageLimit) {
         this.name = name
         this.author = author
         this.year = year
         this.pages = pages
         this.isAllowed = isAllowed
+        this.ageLimit = ageLimit
     }
 
     info() {
-        console.log(`Книга \"${this.name}\" написанна автором ${this.author} в ${this.year} на ${this.pages} страниц`)
+        console.log(`Книга "${this.name}" написанна автором ${this.author} в ${this.year} на ${this.pages} страниц. Возрастное ограничение: ${this.ageLimit}+`)
     }
 
     getBook(person) {
-        if(this.isAllowed) {
-            person.addBook(this)
-            this.isAllowed = false 
-        } else {
+        if (!this.isAllowed) {
             throw new Error('Книга недоступна')
         }
+        if (person.age < this.ageLimit) {
+            throw new Error('Возраст читателя не подходит')
+        }
+        if (person.books.length >= 3) {
+            throw new Error('Нельзя взять больше 3 книг')
+        }
+        person.addBook(this)
+        this.isAllowed = false
     }
 
     returnBook() {
@@ -76,11 +82,14 @@ class Person {
 }
 
 
-const newBook1 = new Book('У лукоморья дуб срубили', 'Mark Strelkov', 2026, 1, true)
+
+const newBook1 = new Book('У лукоморья дуб срубили', 'Mark Strelkov', 2026, 1, true, 12)
 const newPerson = new Person('Alex', 23, 333333, [])
-const newBook2 = new Book('Чернозём', 'Mark Strelkov', 2025, 1112, true)
-const newBook3 = new Book('Китайская газета - Куйвам Куйнам', 'Mark Strelkov', 2024, 132, true)
-const newBook4 = new Book('50 оттенков сероBо', 'Mark Strelkov', 2023, 1100, true)
+const newBook2 = new Book('Чернозём', 'Mark Strelkov', 2025, 1112, true, 18)
+const newBook3 = new Book('Китайская газета - Куйвам Куйнам', 'Mark Strelkov', 2024, 132, true, 0)
+const newBook4 = new Book('50 оттенков сероBо', 'Mark Strelkov', 2023, 1100, true, 18)
+
+
 
 newPerson.getBook(newBook1)
 newPerson.info()
@@ -104,4 +113,3 @@ console.log(`
     `);
 
 console.log(newPerson.hasBook(newBook2));
- 
